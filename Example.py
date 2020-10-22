030f30e779ed686cc0739237e2396d1a004227f1
Cole-Hayson <haysonc@stu.otc.school.nz>
2020-10-23 12:22:01 +1300

    write code for when player guess wrong number

--- Example.py
+++ Example.py
@@ -1,10 +1,9 @@
 # Generates a random number. This will be the number the player has to guess.
 from random import randint
-correct_number = randint(1, 100)
-print(correct_number)
+correct_number = randint(1, 50)
 # Prints Instructions
 print("Welcome To Guess The Number")
-print("The aim of this game it to simply guess a randomly picked number between 1 and 100.")
+print("The aim of this game it to simply guess a randomly picked number between 1 and 50.")
 print("After each guess you will be told if the correct number is higher or lower than your last guess")
 print("You will have a total of 10 guesses and will be awarded points based on how many guesses you take.")
 
@@ -21,10 +20,16 @@ print("Good Luck and Have Fun")
 
 # Variables
 start = input('\nPlease Type "S" To Begin Game')
-guess1 = input("Please Input Your First Guess")
-
+print(correct_number)
 # Once player starts game
-if start == "S":
-    guess1 = input("Please Input Your First Guess")
-    if guess1 == correct_number:
-        print("Wow, that's amazing you got it right first time! \nYou have gained 10 Points!")
+if start.upper() == "S":
+    for i in range(10):
+        print("\nYou are on guess: ", i + 1)
+        guess = int(input("\nPlease Input Your Guess"))
+        if guess == correct_number:
+            print("\nWow, that's amazing you got it right first time! \nYou have gained 10 Points!")
+        elif guess < correct_number:
+            print("Incorrect, the number you entered is lower than the correct number. Try Again.")
+        elif guess > correct_number:
+            print("Incorrect, the number you entered is higher than the correct number. Try Again.")
+    print("Game Over")
