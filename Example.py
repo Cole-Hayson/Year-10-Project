5e2a3fdf740a10d6c4c6667b77d62139289ff218
Cole-Hayson <haysonc@stu.otc.school.nz>
2020-11-10 12:25:05 +1300

    Completed Game

--- Example.py
+++ Example.py
@@ -1,9 +1,9 @@
 # Generates a random number. This will be the number the player has to guess.
-from random import randint
-correct_number = randint(1, 50)
+import random
+
 # Prints Instructions
 print("Welcome To Guess The Number")
-print("The aim of this game it to simply guess a randomly picked number between 1 and 50.")
+print("The aim of this game it to simply guess a randomly picked number between 1 and 40.")
 print("After each guess you will be told if the correct number is higher or lower than your last guess")
 print("You will have a total of 10 guesses and will be awarded points based on how many guesses you take.")
 
@@ -20,40 +20,49 @@ print("Good Luck and Have Fun")
 
 # Variables
 start = input('\nPlease Type "S" To Begin Game')
-print(correct_number)
 total_points = 0
+level = 0
 # Once player starts game
 if start.upper() == "S":
-    for i in range(10):
-        print("\nYou are on guess: ", i + 1)
-        guess_number = (i + 1)
-        guess = int(input("\nPlease Input Your Guess"))
-        if guess == correct_number and guess_number == 10:
-            print("Correct!, that was a close one. + 1 point")
-        if guess == correct_number and guess_number == (5, 9):
-            print("Correct!, + 1 point")
-            total_points += 1
-        elif guess == correct_number and guess_number == 4:
-            print("Correct! + 2 points")
-            total_points += 2
-        elif guess == correct_number and guess_number == 3:
-            print("Correct, you have been granted 3 points.")
-            total_points += 3
-        elif guess == correct_number and guess_number == 2:
-            print("Correct, 5 points gained!")
-            total_points += 5
-        elif guess == correct_number and guess_number == 1:
-            print("Wow! first guess, + 10 points!")
-            total_points += 10
-        elif guess < correct_number and guess_number == (1, 9):
-            print("Incorrect, the number you entered is lower than the correct number. Try Again.")
-        elif guess > correct_number and guess_number == (1, 9):
-            print("Incorrect, the number you entered is higher than the correct number. Try Again.")
-        elif guess != correct_number and guess_number == 10:
-            print("YOU LOSE, GAME OVER \n\nYou had a total of: ", total_points, " points")
+    while level < 6:
+        correct_number = random.randint(1, 40)
+        print(correct_number)
+        for i in range(10):
+            print("\nYou are on guess: ", i + 1)
+            guess_number = (i + 1)
+            guess = int(input("\nPlease Input Your Guess"))
+            if guess == correct_number and guess_number > 4:
+                print("Correct!, + 1 point")
+                total_points += 1
+            elif guess == correct_number and guess_number == 4:
+                print("Correct! + 2 points")
+                total_points += 2
+            elif guess == correct_number and guess_number == 3:
+                print("Correct, you have been granted 3 points.")
+                total_points += 3
+            elif guess == correct_number and guess_number == 2:
+                print("Correct, 5 points gained!")
+                total_points += 5
+            elif guess == correct_number and guess_number == 1:
+                print("Wow! first guess, + 10 points!")
+                total_points += 10
+            elif guess < correct_number and guess_number < 10:
+                print("Incorrect, the number you entered is lower than the correct number. Try Again.")
+            elif guess > correct_number and guess_number < 10:
+                print("Incorrect, the number you entered is higher than the correct number. Try Again.")
 
-        if guess == correct_number:
-            break
+            if guess != correct_number and guess_number == 10:
+                print("\nYOU LOSE, GAME OVER\n\nYou had a total of: ", total_points, " points")
+                print("\nRestarting Game...\n\nClose Game If You Do Not Want To Replay")
+                level -= level
+                total_points -= total_points
 
+            if guess == correct_number:
+                level += 1
+                print("\nYou are on level: ", level)
+                print("\nYou have a total of", total_points, "points")
+                break
 
+            if level == 6:
+                print("You Have Completed Level 5. You Won The Game!")
 
